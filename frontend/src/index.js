21bf0bbe9df372a3cb30418269bcd5ddd9a4a7e4
ChrishanAloka <chrishan2002aloka@gmail.com>
2026-02-17 17:41:17 -0800

    Update and deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 // import { AuthProvider } from "./components/ProtectedRoute";
 import { AuthProvider } from "./context/auth-context";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -17,3 +18,8 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
